5709009defe05dc0201e00c200297e296fefec37
Joe P Shoulak <joe@squarecompass.io>
2022-12-09 15:28:25 -0600

    about to insert into template

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,12 +1,35 @@
 import React from "react";
 import { ReactP5Wrapper } from "react-p5-wrapper";
+import Bloon from "../library/Bloon";
 
 const sketch = (p5) => {
-  p5.setup = () => p5.createCanvas(600, 400);
+  let path = [];
+  let b;
+
+  p5.setup = () => {
+    p5.createCanvas(600, 400);
+    p5.background("green");
+
+    path = [
+      { x: p5.width / 3, y: p5.height },
+      { x: p5.width / 3, y: p5.height / 3 },
+      { x: p5.width, y: p5.height / 3 },
+    ];
+
+    b = new Bloon(p5.width / 2, p5.height / 2);
+  };
 
   p5.draw = () => {
-    p5.background(20);
-    p5.circle(p5.width / 2, p5.height / 2, 10);
+    path.forEach((px, i) => {
+      if (i === 0) return;
+      const prev = path[i - 1];
+
+      p5.line(prev.x, prev.y, px.x, px.y);
+    });
+
+    b.update((b) => {
+      p5.circle(b.pos.x, b.pos.y, 10);
+    });
   };
 };
 
